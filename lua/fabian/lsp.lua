41e38289279bad4d6d168c0155243458ffb48564
FabianTsch <tschohl.fabian@gmail.com>
2022-05-19 20:52:33 +0200

    changed python lsp server

--- lua/fabian/lsp.lua
+++ lua/fabian/lsp.lua
@@ -32,7 +32,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'jedi_language_server', 'clangd', 'bashls' }
+local servers = { 'pylsp', 'clangd', 'bashls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
