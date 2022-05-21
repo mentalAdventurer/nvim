e1827b17d92a4d11f1bb51d8eff7511abc7711e4
FabianTsch <tschohl.fabian@gmail.com>
2022-05-21 21:49:15 +0200

    add autocmpletion

--- lua/fabian/plugins.lua
+++ lua/fabian/plugins.lua
@@ -14,7 +14,7 @@ Plug 'nvim-telescope/telescope-fzy-native.nvim'
 
 -- Snippets
 Plug 'SirVer/ultisnips'
-
+Plug 'quangnguyen30192/cmp-nvim-ultisnips'
 
 -- theme
 Plug 'ellisonleao/gruvbox.nvim'
@@ -23,6 +23,14 @@ Plug 'nvim-lualine/lualine.nvim'
 -- native lsp
 Plug 'neovim/nvim-lspconfig'
 Plug 'honza/vim-snippets'
+
+-- cmp plugins
+Plug "hrsh7th/nvim-cmp" -- The completion plugin
+Plug "hrsh7th/cmp-buffer" -- buffer completions
+Plug "hrsh7th/cmp-path" -- path completions
+Plug "hrsh7th/cmp-cmdline" -- cmdline completions
+Plug "saadparwaiz1/cmp_luasnip" -- snippet completions
+Plug "hrsh7th/cmp-nvim-lsp"
 
 -- treesitter
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
