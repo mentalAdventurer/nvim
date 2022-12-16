daaca5263a7d0eede2ca901903a36ef0b27d6e43
FabianTsch <tschohl.fabian@gmail.com>
2022-12-16 21:22:19 +0100

    changed back to default vim keybindings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -11,20 +11,20 @@ local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = ","
 
 -- change to shifted keybindings
-keymap("", "j", "h", opts)
-keymap("", "k", "j", opts)
-keymap("", "l", "k", opts)
-keymap("", "ö", "l", opts)
-keymap("", "gj", "gh", opts)
-keymap("", "gk", "gj", opts)
-keymap("", "gl", "gk", opts)
-keymap("", "gö", "gl", opts)
+-- keymap("", "j", "h", opts)
+-- keymap("", "k", "j", opts)
+-- keymap("", "l", "k", opts)
+-- keymap("", "ö", "l", opts)
+-- keymap("", "gj", "gh", opts)
+-- keymap("", "gk", "gj", opts)
+-- keymap("", "gl", "gk", opts)
+-- keymap("", "gö", "gl", opts)
 
 -- change window navigation to shifted keybindings
-keymap("n", "<c-w>j", "<c-w>h", opts)
-keymap("n", "<c-w>k", "<c-w>j", opts)
-keymap("n", "<c-w>l", "<c-w>k", opts)
-keymap("n", "<c-w>ö", "<c-w>l", opts) 
+-- keymap("n", "<c-w>j", "<c-w>h", opts)
+-- keymap("n", "<c-w>k", "<c-w>j", opts)
+-- keymap("n", "<c-w>l", "<c-w>k", opts)
+-- keymap("n", "<c-w>ö", "<c-w>l", opts) 
 
 -- adjustment to german keyboard
 keymap("n", "ü", "{", opts)
