60ce7692a0b40b5036ef6ae8eb61712b9f468262
FabianTsch <tschohl.fabian@gmail.com>
2023-12-21 18:38:31 +0100

    remove lua snippets

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -30,7 +30,6 @@ use "hrsh7th/nvim-cmp" -- The completion plugin
 use "hrsh7th/cmp-buffer" -- buffer completions
 use "hrsh7th/cmp-path" -- path completions
 use "hrsh7th/cmp-cmdline" -- cmdline completions
-use "saadparwaiz1/cmp_luasnip" -- snippet completions
 use "hrsh7th/cmp-nvim-lsp"
 
 -- copilot
