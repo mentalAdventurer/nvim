ce0f2e3d8411cfe76870670bafe233abd0d9c3da
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2025-04-17 12:35:51 +0200

    refactor(lsp): update lspconfig for neovim `v0.11.0`

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,52 +1,49 @@
-------------------------------------------------------------------------
---                            LSP-Settings                            --
-------------------------------------------------------------------------
-
--- Mappings
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
+-- Diagnostic mappings
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
--- Bind keys only if Lsp is attached to buffer
--- succesor of on_attach function
+-- Buffer-local mappings on LSP attach
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ds', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>',
-        opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ws',
-        '<cmd>lua require("telescope.builtin").lsp_dynamic_workspace_symbols()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, bufopts)
+    vim.keymap.set('n', 'ds', require('telescope.builtin').lsp_document_symbols, bufopts)
+    vim.keymap.set('n', 'ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, bufopts)
   end,
 })
 
--- Setup of language servers
-require'lspconfig'.pyright.setup{
-    settings = {
-        python = {
-              analysis = {
-                  typeCheckingMode="standard"
-              }
-        },
+
+-- Enaable LSP servers
+vim.lsp.enable('pyright')
+vim.lsp.enable('bashls')
+vim.lsp.enable('clangd')
+vim.lsp.enable('ruff')
+vim.lsp.enable('jsonls')
+
+-- Configure LSP servers
+vim.lsp.config('pyright', {
+  settings = {
+    python = {
+      analysis = { typeCheckingMode = "standard" },
     },
-}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.clangd.setup{}
---require'lspconfig'.ruff.setup{}
-require'lspconfig'.texlab.setup{}
---require'lspconfig'.ruff_lsp.setup{}
-require'lspconfig'.jsonls.setup{}
+  },
+})
