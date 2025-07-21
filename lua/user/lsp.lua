bd854742f737a53e5a12468ff226ec06f639865c
FabianTsch <tschohl.fabian@gmail.com>
2025-07-21 09:08:23 +0200

    perf(lsp): remove dublicated keymaps after updating to `v0.11.0`

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -2,8 +2,6 @@ local opts = { noremap = true, silent = true }
 
 -- Diagnostic mappings
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Buffer-local mappings on LSP attach
@@ -16,18 +14,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, bufopts)
-    vim.keymap.set('n', 'ds', require('telescope.builtin').lsp_document_symbols, bufopts)
-    vim.keymap.set('n', 'ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
   end,
 })
 
@@ -38,12 +26,3 @@ vim.lsp.enable('bashls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('ruff')
 vim.lsp.enable('jsonls')
-
--- Configure LSP servers
-vim.lsp.config('pyright', {
-  settings = {
-    python = {
-      analysis = { typeCheckingMode = "standard" },
-    },
-  },
-})
