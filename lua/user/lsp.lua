3555993be22760f0c4202e440ef67e5b9b7e175f
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2025-08-06 12:30:57 +0200

    style(lsp): apply autoformatting with `stylua`

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,31 +1,30 @@
 local opts = { noremap = true, silent = true }
 
 -- Diagnostic mappings
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Buffer-local mappings on LSP attach
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	callback = function(args)
+		local bufnr = args.buf
+		vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+		local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
-    vim.keymap.set('n', '<leader>fl', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufopts)
-    vim.keymap.set('n', 'grr', require('telescope.builtin').lsp_references, bufopts)
-  end,
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, bufopts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+		vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+		vim.keymap.set("n", "<leader>fl", require("telescope.builtin").lsp_dynamic_workspace_symbols, bufopts)
+		vim.keymap.set("n", "grr", require("telescope.builtin").lsp_references, bufopts)
+	end,
 })
 
-
 -- Enaable LSP servers
-vim.lsp.enable('pyright')
-vim.lsp.enable('bashls')
-vim.lsp.enable('clangd')
-vim.lsp.enable('ruff')
-vim.lsp.enable('jsonls')
-vim.lsp.enable('fish_lsp')
+vim.lsp.enable("pyright")
+vim.lsp.enable("bashls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("ruff")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("fish_lsp")
