417bdf8a425583ee0b09b7809d7ba47369c04517
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2026-04-29 09:36:37 +0200

    feat(diffview): add `diff1_inline` to `cycle_layouts`

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,6 +1,13 @@
 return {
 	"dlyongemallo/diffview.nvim",
 	version = "*",
+	opts = {
+		view = {
+			cycle_layouts = {
+				default = { "diff2_horizontal", "diff1_inline" },
+			},
+		},
+	},
 	-- optional: lazy-load on command
 	-- cmd = {
 	--     "DiffviewOpen",
