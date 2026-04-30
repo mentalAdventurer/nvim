e586d15d22e5cfea4ffc168a231fcf786bfc4dfb
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2026-04-30 12:28:21 +0200

    feat(gitsigins): add keymaps for stage, unstage and reset hunk

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,5 +4,32 @@ return {
 		config = function()
 			require("gitsigns").setup()
 		end,
+		keys = {
+			{
+				"<leader>hs",
+				function()
+					require("gitsigns").stage_hunk()
+				end,
+				desc = "Stage hunk",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>hr",
+				function()
+					require("gitsigns").reset_hunk()
+				end,
+				desc = "Reset hunk",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>hu",
+				function()
+					require("gitsigns").undo_stage_hunk()
+				end,
+				desc = "Undo stage hunk",
+				mode = { "n", "v" },
+			},
+		},
+		lazy = false,
 	},
 }
